ae6092dbfcf8991628dfd4c7b2e7c27a4de721b6
iz <43661631+mencretsu@users.noreply.github.com>
2024-05-06 05:47:47 +0700

    Update vascan.sol

--- vascan.sol
+++ vascan.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+
 interface IBEP20 {
     function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
